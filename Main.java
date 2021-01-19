ca6cacba3d353e97b70fc5c03a052560d5fd102a
Jin De <91093935@g.mvusd.net>
2021-01-19 19:38:35 +0000

    Add Second button XD

--- Main.java
+++ Main.java
@@ -11,6 +11,8 @@ public class Main
     frame1.setSize(500,500);
     frame1.setVisible(true);
 
+    
+
     //Button
     JButton button1 = new JButton("Click me");
     button1.addActionListener(new ActionListener()
@@ -21,7 +23,18 @@ public class Main
       }//end the block of what button will do
     });//end the action listener
 
+    //Button 2
+    JButton button2 = new JButton("NO CLICK ME");
+    button2.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        System.out.println("Thanks Pal");
+      }//end what button does
+    });//end the action listener
+
     frame1.add(button1);
+    frame1.add(button2);
     frame1.setVisible(true);
   }//end main method
 }//end class Main
